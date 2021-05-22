e746b950b46310ac4e266ecacaa62ff7d21a2b7e
ctle-vn <ctle@sfu.ca>
2021-05-22 21:28:20 +0800

    Added Dex tests.

--- test/Dex.test.js
+++ test/Dex.test.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai");
+
 require("chai")
   .use(require("bn-chai")(web3.utils.BN))
   .use(require("chai-as-promised"))
@@ -15,6 +17,36 @@ contract("IntegratedLimitOrderDex", (accounts) => {
   });
 
   it("should support placing sell orders", async () => {
+    const oldBalance = (await dex.balanceOf.call(accounts[0]))
     await dex.sell(100, 10);
+    const newBalance = (await dex.balanceOf.call(accounts[0]))
+    assert.equal(newBalance, oldBalance - 100, "Sell order did not update balance")
+  });
+  
+  it("should support placing multiple buy orders", async () => {
+    const amount = 1;
+    const oneWei = web3.utils.toWei("1");
+    const price = web3.utils.toBN(oneWei);
+    const numBuys = 5;
+
+    for (let i = 0; i < numBuys; i++) {
+      await dex.buy(amount, price, {value: oneWei});
+    }
+
+    let orderQuantity = (await dex.getOrderQuantity(price))
+    assert.equal(orderQuantity, numBuys, "Buy order did not update balance")
+  });
+
+  it("should support fully filling sell order", async () => {
+    const oneWei = web3.utils.toWei("1");
+    const price = web3.utils.toBN(oneWei);
+    const amount = 5;
+
+    await dex.sell(amount, price);
+
+    let orderQuantity = (await dex.getOrderQuantity(price))
+    let balance = (await dex.getEthBalance(accounts[0]))
+    assert.equal(orderQuantity, 0, "Order quantity did not update balance")
+    assert.equal(balance, price * 5, "Ethereum balance did not update")
   });
 });
