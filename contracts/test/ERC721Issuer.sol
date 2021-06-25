3be9d21d42429afbd349dfa4f83709c107363e7e
Tom Wilson <tom.wilson87@abine.us>
2021-06-25 08:04:04 -0500

    Lint the ERC721Issuer contract.

--- contracts/test/ERC721Issuer.sol
+++ contracts/test/ERC721Issuer.sol
@@ -2,11 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../asset/IntegratedLimitOrderDex.sol";
-
-contract StubbedDex is ERC721 {
-
-  constructor() ERC721("NFT ERC721", "FFTKN") {}
+contract ERC721Issuer is ERC721 {
+  constructor() ERC721("NFT ERC721", "721") {}
 
   function mint(address to, uint256 tokenId) public {
       _safeMint(to, tokenId);
