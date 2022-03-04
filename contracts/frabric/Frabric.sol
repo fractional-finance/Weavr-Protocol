735d6c7f9412f2546946a2faf234a546ad092c70
Tom Wilson <tom.wilson87@abine.us>
2022-03-04 11:34:07 -0500

    Add Thread proposals to the Frabric
    Also moves from using FrabricERC20 to IFrabricERC20

--- contracts/frabric/Frabric.sol
+++ contracts/frabric/Frabric.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "../erc20/FrabricERC20.sol";
+import "../interfaces/erc20/IFrabricERC20.sol";
+import "../thread/ThreadDeployer.sol";
 
 import "../dao/DAO.sol";
 
@@ -24,7 +25,7 @@ contract Frabric is DAO {
   }
   mapping(uint256 => Participant) internal _participants;
   event ParticipantProposed(uint256 indexed id, address participant, uint256 indexed participantType);
-  event ParticipantUpdated(uint256 indexed id, address participant);
+  event ParticipantUpdated(uint256 indexed id, address participant, uint256 indexed participantType);
 
   enum GuardianStatus {
     Null,
@@ -34,8 +35,20 @@ contract Frabric is DAO {
   }
   mapping(address => GuardianStatus) public guardian;
 
+  struct ThreadProposal {
+    string name;
+    string symbol;
+    address agent;
+    address raiseToken;
+    uint256 target;
+  }
+  mapping(uint256 => ThreadProposal) internal _threads;
+  event ThreadProposed(uint256 indexed id, address indexed agent, address indexed raiseToken, uint256 target);
+
+  address public threadDeployer;
+
   modifier beforeProposal() {
-    require(FrabricERC20(erc20).whitelisted(msg.sender), "Frabric: Proposer isn't whitelisted");
+    require(IFrabricERC20(erc20).whitelisted(msg.sender), "Frabric: Proposer isn't whitelisted");
     _;
   }
 
@@ -48,15 +61,30 @@ contract Frabric is DAO {
     return _createProposal(info, 0);
   }
 
+  function proposeThread(
+    string calldata info,
+    string memory name,
+    string memory symbol,
+    address agent,
+    address raiseToken,
+    uint256 target
+  ) external beforeProposal() returns (uint256) {
+    require(bytes(name).length >= 3, "Frabric: Thread name has less than three characters");
+    require(bytes(symbol).length >= 2, "Frabric: Thread symbol has less than two characters");
+    require(guardian[agent] == GuardianStatus.Active, "Frabric: Guardian selected to be agent isn't active");
+    _threads[_nextProposalID] = ThreadProposal(name, symbol, agent, raiseToken, target);
+    emit ThreadProposed(_nextProposalID, agent, raiseToken, target);
+    return _createProposal(info, 1);
+  }
+
   function _completeProposal(uint256 id, uint256 proposalType) internal override {
-    Participant storage participant = _participants[id];
-    participant.passed = true;
     if (proposalType == 0) {
+      Participant storage participant = _participants[id];
+      participant.passed = true;
       if (participant.participantType == ParticipantType.KYC) {
         emit KYCChanged(kyc, participant.participant);
         kyc = participant.participant;
       } else {
-        emit ParticipantUpdated(id, participant.participant);
         if (participant.participantType == ParticipantType.Guardian) {
           require(guardian[participant.participant] == GuardianStatus.Null);
           guardian[participant.participant] = GuardianStatus.Unverified;
@@ -65,9 +93,14 @@ contract Frabric is DAO {
             guardian[participant.participant] = GuardianStatus.Removed;
           }
           // Remove them from the whitelist
-          FrabricERC20(erc20).setWhitelisted(participant.participant, bytes32(0));
+          IFrabricERC20(erc20).setWhitelisted(participant.participant, bytes32(0));
         }
+        emit ParticipantUpdated(id, participant.participant, uint256(participant.participantType));
       }
+    } else if (proposalType == 1) {
+      ThreadProposal memory proposal = _threads[id];
+      // erc20 here is used as the parent whitelist as it's built into the Frabric ERC20
+      ThreadDeployer(threadDeployer).deploy(proposal.name, proposal.symbol, erc20, proposal.agent, proposal.raiseToken, proposal.target);
     }
   }
 
@@ -76,6 +109,6 @@ contract Frabric is DAO {
     require(_participants[id].passed);
     require(!_participants[id].whitelisted);
     _participants[id].whitelisted = true;
-    FrabricERC20(erc20).setWhitelisted(_participants[id].participant, kycHash);
+    IFrabricERC20(erc20).setWhitelisted(_participants[id].participant, kycHash);
   }
 }
--- contracts/interfaces/lists/IFrabricWhitelistExposed.sol
+++ contracts/interfaces/lists/IFrabricWhitelistExposed.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: AGPLv3
 pragma solidity >=0.8.9;
 
-interface IFrabricWhitelistExposed {
+import "./IGlobalWhitelist.sol";
+
+interface IFrabricWhitelistExposed is IGlobalWhitelist {
   function setParentWhitelist(address whitelist) external;
   function setWhitelisted(address person, bytes32 dataHash) external;
   function globallyAccept() external;
