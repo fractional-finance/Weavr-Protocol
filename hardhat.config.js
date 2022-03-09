86e1711d11d3d79a27928b6321eadc1b02f8ae56
Tom Wilson <tom.wilson87@abine.us>
2022-03-09 10:42:32 -0500

    Replace npm with yarn
    Corrects dependencies, purging truffle

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,14 @@
-const { task } = require('hardhat/config');
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-// temporary until complete conversion
-require("@nomiclabs/hardhat-truffle5");
-// TBR to be reaearch on
 require("@nomiclabs/hardhat-waffle");
-// Solidy Coverage plugin
 require("solidity-coverage");
 
-// Basic task example
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
