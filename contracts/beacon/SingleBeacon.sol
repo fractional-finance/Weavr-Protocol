2b3f922256efed7ddce1745563b8f9d197a1d633
Tom Wilson <tom.wilson87@abine.us>
2022-03-19 19:51:13 -0400

    Document SingleBeacon

--- contracts/beacon/SingleBeacon.sol
+++ contracts/beacon/SingleBeacon.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.9;
 
 import "./Beacon.sol";
 
+// A pointless Beacon which reduces a BeaconProxy to a normal upgradable proxy
+// Maintains consistency of using BeaconProxys which may have benefits in the future
+// This can be chained with a regular Beacon to become a regular Beacon which BeaconProxys could actually take advantage of
 contract SingleBeacon is Beacon {
-  constructor(uint8 _releaseChannels) Beacon(_releaseChannels) {}
+  constructor() Beacon(1) {}
 
   function upgrade(address instance, address code) public override {
     require(instance == address(0), "SingleBeacon: Can only upgrade the release channel");
