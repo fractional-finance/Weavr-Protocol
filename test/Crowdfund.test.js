582009a8c8e358d63cabd236c06d894f0d026fe3
abstrucked <mxhgraphics@gmail.com>
2022-04-09 15:21:23 +0200

    wip

--- test/Crowdfund.test.js
+++ test/Crowdfund.test.js
@@ -12,58 +12,62 @@ const State = {
 }
 
 let crowdfund;
-  let target;
-  let erc20Beacon;
-  let data;
-  let signer;
-  let otherUser;
-  let testFrabric;
-  let threadDeployer;
-  let auction = {
-    address: "0x0000000000000000000000000000000000000000"
-  };
+let target;
+let erc20;
+let data;
+let agent;
+let user1;
+let user2;
+let user3;
+let testFrabric;
+let threadDeployer;
+let auction = {
+  address: "0x0000000000000000000000000000000000000000"
+};
 
-  let stateCounter;
+let stateCounter;
 
-  async function init(){
-    const Erc20 = await ethers.getContractFactory("TestERC20");
-    erc20 = await Erc20.deploy("Test", "T");
-    await erc20.deployed();
+async function init(){
+  const Erc20 = await ethers.getContractFactory("TestERC20");
+  erc20 = await Erc20.deploy("Test", "T");
+  await erc20.deployed();
 
-    erc20Beacon = await FrabricERC20.deployBeacon();
-    threadDeployer = await ThreadDeployer(erc20Beacon.address, auction.address);
+  let erc20Beacon = await FrabricERC20.deployBeacon();
+  threadDeployer = await ThreadDeployer(erc20Beacon.address, auction.address);
 
-    [signer, otherUser] = await ethers.getSigners();
+  [agent, user1, user2, user3] = await ethers.getSigners();
 
-    target = ethers.BigNumber.from("1000").toString();
-    const ABIC = ethers.utils.defaultAbiCoder;
-    data = ABIC.encode(
-      ["address", "uint256"],
-      [erc20.address, target]
-    );
-    
-    const TestFrabric = await ethers.getContractFactory("TestFrabric");
-    testFrabric = await TestFrabric.deploy();
-    await testFrabric.deployed();
-    await testFrabric.setWhitelisted(otherUser.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
-    await testFrabric.setWhitelisted(signer.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
-    
-    await threadDeployer.threadDeployer.transferOwnership(testFrabric.address);
-    
-    const res = await testFrabric.threadDeployDeployer(
-      threadDeployer.threadDeployer.address,
-      0,
-      signer.address,
-      "TestThread",
-      "TT",
-      data
-    );
-    const add = (await threadDeployer.threadDeployer.queryFilter(threadDeployer.threadDeployer.filters.Thread()))[0].args.crowdfund;
-    const Crowdfund = await ethers.getContractFactory("Crowdfund");
-    crowdfund = Crowdfund.attach(add);
-  }
+  target = ethers.BigNumber.from("1000").toString();
+  const ABIC = ethers.utils.defaultAbiCoder;
+  data = ABIC.encode(
+    ["address", "uint256"],
+    [erc20.address, target]
+  );
+  
+  const TestFrabric = await ethers.getContractFactory("TestFrabric");
+  testFrabric = await TestFrabric.deploy();
+  await testFrabric.deployed();
+  await testFrabric.setWhitelisted(user1.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  await testFrabric.setWhitelisted(user2.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  await testFrabric.setWhitelisted(user3.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  await testFrabric.setWhitelisted(agent.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  
+  await threadDeployer.threadDeployer.transferOwnership(testFrabric.address);
+  
+  const res = await testFrabric.threadDeployDeployer(
+    threadDeployer.threadDeployer.address,
+    0,
+    agent.address,
+    "TestThread",
+    "TT",
+    data
+  );
+  const add = (await threadDeployer.threadDeployer.queryFilter(threadDeployer.threadDeployer.filters.Thread()))[0].args.crowdfund;
+  const Crowdfund = await ethers.getContractFactory("Crowdfund");
+  crowdfund = Crowdfund.attach(add);
+}
 
-describe("Crowdfund Positive Test Cases", async  () => {
+describe("Happy Path", async  () => {
   
   describe("Test until finsish+burn", async () => {
     before(async () => {
@@ -79,12 +83,16 @@ describe("Crowdfund Positive Test Cases", async  () => {
     });
     it("Should deposit tokens as a new backer", async () => {    
   
-      const balance = await erc20.balanceOf(signer.address);
+      const balance = {
+        agent: await erc20.balanceOf(agent.address),
+        user1: await erc20.balanceOf(user1.address),
+      }
       const amount = ethers.BigNumber.from("100");
       
-      await erc20.approve(crowdfund.address, balance);
-      
-      const tx = await crowdfund.deposit(amount);
+      await erc20.approve(crowdfund.address, balance.agent);
+      await erc20.transfer(user1.address, amount);
+      await erc20.connect(user1).approve(crowdfund.address, balance.agent);
+      const tx = await crowdfund.connect(user1).deposit(amount);
       expect(tx).to.emit(Crowdfund, "Deposit");
 
       const event = (await crowdfund.queryFilter(crowdfund.filters.Deposit()));
@@ -94,7 +102,7 @@ describe("Crowdfund Positive Test Cases", async  () => {
     it("Should withdraw tokens as an existing backer", async () => {
       
       const amount = ethers.BigNumber.from("100");
-      const tx = await crowdfund.withdraw(amount);
+      const tx = await crowdfund.connect(user1).withdraw(amount);
       
       expect(tx).to.emit(Crowdfund, "Withdraw");
       
@@ -114,10 +122,11 @@ describe("Crowdfund Positive Test Cases", async  () => {
       stateCounter++;
       const event = (await crowdfund.queryFilter(crowdfund.filters.StateChange()));
       expect(event[stateCounter].args.state).to.equal(State.Executing);
+
     });
     it("Should transfer total balance to agent when state changes to executing status", async () => {
       
-      const agentBalance = await crowdfund.balanceOf(signer.address);
+      const agentBalance = await crowdfund.balanceOf(agent.address);
       expect(agentBalance).to.equal(target);
   
     });
@@ -132,7 +141,7 @@ describe("Crowdfund Positive Test Cases", async  () => {
     });
     it("Should burn backer crowdfund tokens when state changes to finished status", async () => {
   
-      const depositor = signer.address;
+      const depositor = agent.address;
       const balance = await crowdfund.balanceOf(depositor);
       const tx = await crowdfund.burn(depositor);
       expect(await crowdfund.balanceOf(depositor)).to.equal(0);
@@ -149,19 +158,20 @@ describe("Crowdfund Positive Test Cases", async  () => {
   });
   describe("Implementation to refound a Crowdfund", async () => {
     before(async () => {
-
-      await init();
-     
       
+      await init(); 
       stateCounter = 0;
+
     });
     it("Should allow agent to trigger refunding status if was previously executing", async () => {
-      const balance = await erc20.balanceOf(signer.address);
+      
+      const balance = await erc20.balanceOf(agent.address);
       const amount = ethers.BigNumber.from(target);
       
       await erc20.approve(crowdfund.address, balance);
-      
-      const tx = await crowdfund.deposit(amount);
+      await erc20.connect(user1).approve(crowdfund.address, balance);
+      await erc20.transfer(user1.address, amount);
+      const tx = await crowdfund.connect(user1).deposit(amount);
       
       const tx1 = await crowdfund.execute();
       expect(tx1).to.emit(Crowdfund, "StateChange");
@@ -173,10 +183,19 @@ describe("Crowdfund Positive Test Cases", async  () => {
       const state = (await crowdfund.queryFilter(crowdfund.filters.Distributed()));
       
       expect(state[0].args.amount).to.equal(amount);
+
     });
     it("Should allow backers to claim their funds if the crowdfund status is refunding, and burn their crowdfund ERC20 tokens", async () => {
+      
       const event = (await crowdfund.queryFilter(crowdfund.filters.StateChange()));
       expect(event[stateCounter].args.state).to.equal(State.Refunding);
+      const dist = (await crowdfund.queryFilter(crowdfund.filters.Distributed()));
+      console.log(await crowdfund.connect(user1).balanceOf(user1.address));
+      const ID = (dist[0].args.id);
+      console.log(dist[0].args.amount);
+      const tx = await crowdfund.connect(user1).claim(user1.address, ID);
+      expect(tx).emit(Crowdfund, "Claimed");
+    
     });
     
   });
@@ -186,15 +205,38 @@ describe("Crowdfund Positive Test Cases", async  () => {
 });
 
 describe("Crowdfund Negative Test Cases", accounts => {
-  let crowdfund;
-  before(async function () {
+  beforeEach(async function () {
    await init();
+   stateCounter = 0;
   });
     
-    it("Should not be allowed to back a crowdfund that has ended", async () => {});
-    it("Should not be allowed to withdraw funds from a crowdfund that is executing", async () => {});
-    it("Should not be allowed to withdraw funds from a crowdfund that has ended", async () => {});
-    it("Should not launch a crowdfund with a zero fundraising target", async () => {
-      // let crowdfund =  await ethers.getContractFactory("ERC20");
-    });
+  it("Should not be allowed to back a crowdfund that has ended", async () => {
+    const balance = await erc20.balanceOf(agent.address);
+    const amount = ethers.BigNumber.from(target);
+    
+    await erc20.approve(crowdfund.address, balance);
+    
+    const tx = await crowdfund.deposit(amount);
+    
+    const tx1 = await crowdfund.execute();
+    expect(tx1).to.emit(Crowdfund, "StateChange");
+    stateCounter++;
+    const tx2 = await crowdfund.finish();
+    expect(tx2).to.emit(Crowdfund, "StateChange");
+    stateCounter++;
+    const event = (await crowdfund.queryFilter(crowdfund.filters.StateChange()));
+    expect(event[stateCounter].args.state).to.equal(State.Finished);
+    // const tx3 = await crowdfund.deposit(amount);
+    expect(
+      await crowdfund.deposit(amount)
+    )
+    .to.be.revertedWith(
+      "'InvalidState(3, 0)'"
+    );
+  });
+  it("Should not be allowed to withdraw funds from a crowdfund that is executing", async () => {});
+  it("Should not be allowed to withdraw funds from a crowdfund that has ended", async () => {});
+  it("Should not launch a crowdfund with a zero fundraising target", async () => {
+    // let crowdfund =  await ethers.getContractFactory("ERC20");
+  });
 });
--- test/Thread.test.js
+++ test/Thread.test.js
@@ -1,252 +1,193 @@
-/*
-const { assert } = require("chai");
+.002222222
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const FrabricERC20 = require("../scripts/deployFrabricERC20.js");
+const Thread = require("../scripts/deployThread.js");
+const ThreadDeployer = require("../scripts/deployThreadDeployer.js");
 
-require("chai")
-  .use(require("bn-chai")(require("web3").utils.BN))
-  .use(require("chai-as-promised"))
-  .should();
+let thread;
+let target;
+let erc20;
+let data;
+let agent;
+let user1;
+let user2;
+let user3;
+let testFrabric;
+let threadDeployer;
+let auction = {
+  address: "0x0000000000000000000000000000000000000000"
+};
 
-let StubbedDex = ethers.getContractFactory("FrabricERC20");
+let stateCounter;
 
-contract("FrabricERC20", (accounts) => {
-  let dex;
+async function init(){
+  const Erc20 = await ethers.getContractFactory("TestERC20");
+  erc20 = await Erc20.deploy("Test", "T");
+  await erc20.deployed();
 
-  it("should mint tokens to test with", async () => {
-    dex = await FrabricERC20.deploy();
-    (await dex.totalSupply.call()).should.be.eq.BN(await dex.balanceOf.call(accounts[0]));
-    (await dex.totalSupply.call()).toString().should.be.equal("1000000000000000000");
-    await dex.approve(dex.address, await dex.totalSupply.call());
+  let erc20Beacon = await FrabricERC20.deployBeacon();
+  threadDeployer = await ThreadDeployer(erc20Beacon.address, auction.address);
+
+  [agent, user1, user2, user3] = await ethers.getSigners();
+
+  target = ethers.BigNumber.from("1000").toString();
+  const ABIC = ethers.utils.defaultAbiCoder;
+  data = ABIC.encode(
+    ["address", "uint256"],
+    [erc20.address, target]
+  );
+  
+  const TestFrabric = await ethers.getContractFactory("TestFrabric");
+  testFrabric = await TestFrabric.deploy();
+  await testFrabric.deployed();
+  await testFrabric.setWhitelisted(user1.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  await testFrabric.setWhitelisted(user2.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  await testFrabric.setWhitelisted(user3.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  await testFrabric.setWhitelisted(agent.address, "0x0000000000000000000000000000000000000000000000000000000000000001");
+  
+  await threadDeployer.threadDeployer.transferOwnership(testFrabric.address);
+  
+  const res = await testFrabric.threadDeployDeployer(
+    threadDeployer.threadDeployer.address,
+    0,
+    agent.address,
+    "TestThread",
+    "TT",
+    data
+  );
+  const add = (await threadDeployer.threadDeployer.queryFilter(threadDeployer.threadDeployer.filters.Thread()))[0].args.thread;
+  const Thread = await ethers.getContractFactory("Thread");
+  thread = Thread.attach(add);
+}
+
+describe("Thread Happy-Path", async () => {
+  before(async () => {
+    await init();
+  });
+  it("Should initialize a Thread changing emitting AgentChanged and FrabricChanged", async () => {
+    
+    expect(threadDeployer.threadDeployer).to.emit(Thread, "AgentChanged");
+    expect(threadDeployer.threadDeployer).to.emit(Thread, "FrabricChanged");
+    
   });
 
-  it("should support placing sell orders", async () => {
-    const oldBalance = (await dex.balanceOf.call(accounts[0]))
-    let tx = await dex.sell(100, 10);
-    const newBalance = (await dex.balanceOf.call(accounts[0]))
-
-    assert.equal(newBalance, oldBalance - 100, "Sell order did not update balance")
-    assert.equal(tx.logs[2].event, "NewSellOrder", "Incorrect event")
-    assert.equal(tx.logs[2].args.price, 10, "Incorrect event")
-    assert.equal(tx.logs[3].event, "OrderIncrease", "Incorrect event")
-    assert.equal(tx.logs[3].args.sender, accounts[0], "Incorrect event")
-    assert.equal(tx.logs[3].args.price, 10, "Incorrect event")
-    assert.equal(tx.logs[3].args.amount, 100, "Incorrect event")
-  });
-
-  it("should support placing multiple buy orders", async () => {
-    const price = web3.utils.toWei("1");
-    const numBuys = 5;
-
-    let tx;
-    for (let i = 0; i < numBuys; i++) {
-      tx = await dex.buy(1, price, {value: price});
-    }
-
-    let orderQuantity = (await dex.getOrderQuantity(price));
-    assert.equal(orderQuantity, numBuys, "Buy order did not update balance");
-    assert.equal(tx.logs[0].event, "OrderIncrease", "Unexpected event emitted");
-    assert.equal(tx.logs[0].args.price, price, "Unexpected price from event emitted");
-    assert.equal(tx.logs[0].args.amount, 1, "Unexpected amount from event emitted");
-    assert.equal(tx.logs[0].args.sender, accounts[0], "Unexpected sender from event emitted");
-  });
-
-  it("should support fully filling sell order", async () => {
-    const price = web3.utils.toWei("1");
-    const amount = 5;
-
-    let tx = await dex.sell(amount, price);
-
-    let orderQuantity = (await dex.getOrderQuantity(price))
-    let balance = (await dex.getEthBalance(accounts[0]))
-    assert.equal(orderQuantity, 0, "Order quantity did not update balance")
-    assert.equal(balance, price * 5, "Ethereum balance did not update")
-  });
-
-  it("should support partially filled sell orders", async () => {
-    const amount = 1;
-    const price = web3.utils.toWei("1");
-    const numBuys = 5;
-    let oldBalance = (await dex.getEthBalance(accounts[0]))
-
-    for (let i = 0; i < numBuys; i++) {
-      await dex.buy(amount, price, {value: price});
-    }
-
-    const sellAmount = 4
-    await dex.sell(sellAmount, price);
-
-    let orderQuantity = (await dex.getOrderQuantity(price))
-    let balance = (await dex.getEthBalance(accounts[0]))
-    assert.equal(orderQuantity, numBuys - sellAmount, "Order quantity did not update balance")
-    assert.equal(balance - oldBalance, price * sellAmount, "Ethereum balance did not update")
-  });
-
-  it("should support placing multiple new sell orders", async () => {
-    const amount = 1;
-    const price = web3.utils.toWei("2");
-    let numSells = 4;
-
-    let tx;
-    for (let i = 0; i < numSells; i++) {
-      if (i == 0) {
-        tx = await dex.sell(amount, price);
-      } else {
-        await dex.sell(amount, price);
-      }
-    }
-    let orderQuantity = (await dex.getOrderQuantity(price))
-
-    assert.equal(orderQuantity, numSells, "Sell order did not update balance")
-    assert.equal(tx.logs[2].event, "NewSellOrder", "Unexpected event emitted")
-    assert.equal(tx.logs[3].event, "OrderIncrease", "Unexpected event emitted")
-  });
-
-  it("should support placing partially filling buy orders from sell", async () => {
-    const buyAmount = 5;
-    const price = web3.utils.toWei("2");
-
-    let tx = await dex.buy(buyAmount, price, {value: buyAmount * price});
-    let orderQuantity = (await dex.getOrderQuantity(price))
-
-    assert.equal(orderQuantity, 1, "Buy order did not update quantity")
-    assert.equal(tx.logs[0].event, "Filled", "Unexpected event emitted")
-    assert.equal(tx.logs[0].args.sender, accounts[0], "Unexpected sender from event emitted")
-    assert.equal(tx.logs[0].args.recipient, accounts[0], "Unexpected recipient from event emitted")
-    assert.equal(tx.logs[0].args.amount, buyAmount, "Unexpected amount from event emitted")
-    assert.equal(tx.logs[0].args.price, price, "Unexpected price from event emitted")
-    assert.equal(tx.logs[1].event, "Filled", "Unexpected event emitted")
-    assert.equal(tx.logs[1].args.sender, accounts[0], "Unexpected sender from event emitted")
-    assert.equal(tx.logs[1].args.recipient, accounts[0], "Unexpected recipient from event emitted")
-    assert.equal(tx.logs[1].args.price, price, "Unexpected price from event emitted")
-    assert.equal(tx.logs[1].args.amount, buyAmount, "Unexpected amount from event emitted")
-    assert.equal(tx.logs[2].event, "Filled", "Unexpected event emitted")
-    assert.equal(tx.logs[3].event, "Filled", "Unexpected event emitted")
-    assert.equal(tx.logs[4].event, "Transfer", "Unexpected event emitted")
-    assert.equal(tx.logs[4].args.to, accounts[0], "Unexpected recipient from event emitted")
-    assert.equal(tx.logs[5].event, "NewBuyOrder", "Unexpected event emitted")
-    assert.equal(tx.logs[5].args.price, price, "Unexpected price from event emitted")
-    assert.equal(tx.logs[6].event, "OrderIncrease", "Unexpected event emitted")
-    assert.equal(tx.logs[6].args.sender, accounts[0], "Unexpected sender from event emitted")
-    assert.equal(tx.logs[6].args.price, price, "Unexpected price from event emitted")
-    assert.equal(tx.logs[6].args.amount, buyAmount, "Unexpected amount from event emitted")
-  });
-
-  it("should support sell/buy pair across multiple account", async () => {
-    const amount = 1;
-    const price = web3.utils.toWei("3");
-    const numBuys = 5;
-    let oldTokenBalance = (await dex.balanceOf.call(accounts[1]))
-
-
-    await dex.sell(numBuys, price);
-    let tx;
-    for (let i = 0; i < numBuys; i++) {
-      tx = await dex.buy(amount, price, {from: accounts[1], value: price * amount});
-    }
-
-    let newTokenBalance = (await dex.balanceOf.call(accounts[1]))
-    let orderQuantity = (await dex.getOrderQuantity(price))
-
-    assert.equal(oldTokenBalance, 0, "Should have initially a token balance of 0")
-    assert.equal(newTokenBalance, numBuys, "Token balance did not update")
-    assert.equal(orderQuantity, 0, "Order quantity did not update balance")
-    assert.equal(tx.logs[0].event, "Filled", "Unexpected event emitted")
-    assert.equal(tx.logs[0].args.sender, accounts[1], "Unexpected sender from event emitted")
-    assert.equal(tx.logs[0].args.recipient, accounts[0], "Unexpected recipient from event emitted")
-    assert.equal(tx.logs[0].args.amount, amount, "Unexpected amount from event emitted")
-    assert.equal(tx.logs[0].args.price, price, "Unexpected price from event emitted")
-  });
 });
+  // it("should mint tokens to test with", async () => {
+  // });
 
-contract("IntegratedLimitOrderDex", (accounts) => {
-  let dex;
-  beforeEach(async () => {
-    dex = await StubbedDex.new();
-    (await dex.totalSupply.call()).should.be.eq.BN(await dex.balanceOf.call(accounts[0]));
-    (await dex.totalSupply.call()).toString().should.be.equal("1000000000000000000");
-    await dex.approve(dex.address, await dex.totalSupply.call());
-  });
+  // it("should support placing sell orders", async () => {
+  // });
 
-  it("should prevent cheating when buying", async () => {
-    try {
-      const price = web3.utils.toBN(web3.utils.toWei("1"));
-      await dex.buy(1, price, {value: price.div(web3.utils.toBN(2))});
-      assert.fail("should prevent cheating when value is not equal to amount purchased")
-    } catch(err) {
-      const expectedError = "IntegratedLimitOrderDex: Invalid message value"
-      const actualError = err.reason;
-      assert.equal(actualError, expectedError, "should not be permitted")
-    }
-  });
+  // it("should support placing multiple buy orders", async () => {
+  // });
 
-  it("should prevent cheating when selling", async () => {
-    try {
-      const price = web3.utils.toBN(web3.utils.toWei("1"));
-      await dex.sell(1, price, {from: accounts[1]});
-      assert.fail("should prevent cheating when value is not equal to amount purchased")
-    } catch(err) {
-      const expectedError = "ERC20: transfer amount exceeds balance"
-      const actualError = err.reason;
-      assert.equal(actualError, expectedError, "should not be permitted")
-    }
-  });
+  // it("should support fully filling sell order", async () => {
+  // });
 
-  it("should prevent overpaying when buying", async () => {
-    try {
-      const price = web3.utils.toBN(web3.utils.toWei("1"));
-      await dex.buy(1, price, {value: price.mul(web3.utils.toBN(2))});
-      assert.fail("should prevent overpaying when value is not equal to amount purchased")
-    } catch(err) {
-      const expectedError = "IntegratedLimitOrderDex: Invalid message value"
-      const actualError = err.reason;
-      assert.equal(actualError, expectedError, "should not be permitted")
-    }
-  });
+  // it("should support partially filled sell orders", async () => {
+  // });
 
-  it("should prevent 0 buy price", async () => {
-    try {
-      const price = web3.utils.toWei("0.5")
-      await dex.buy(1, 0, {value: 2 * price});
-      assert.fail("should prevent 0 buy price");
-    } catch(err) {
-      const expectedError = "IntegratedLimitOrderDex: Price is 0"
-      const actualError = err.reason;
-      assert.equal(actualError, expectedError, "should not be permitted")
-    }
-  });
+  // it("should support placing multiple new sell orders", async () => {
+  // });
 
-  it("should prevent 0 buy amount", async () => {
-    try {
-      const price = web3.utils.toWei("0.5")
-      await dex.buy(0, price, {value: 2 * price});
-      assert.fail("should prevent 0 buy amount");
-    } catch(err) {
-      const expectedError = "IntegratedLimitOrderDex: Amount is 0"
-      const actualError = err.reason;
-      assert.equal(actualError, expectedError, "should not be permitted")
-    }
-  });
+  // it("should support placing partially filling buy orders from sell", async () => {
+  // });
 
-  it("should prevent 0 sell price", async () => {
-    try {
-      await dex.sell(1, 0);
-      assert.fail("should prevent 0 buy price");
-    } catch(err) {
-      const expectedError = "IntegratedLimitOrderDex: Price is 0"
-      const actualError = err.reason;
-      assert.equal(actualError, expectedError, "should not be permitted")
-    }
-  });
+  // it("should support sell/buy pair across multiple account", async () => {
+  // });
 
-  it("should prevent 0 sell amount", async () => {
-    try {
-      const price = web3.utils.toWei("0.5")
-      await dex.buy(0, price);
-      assert.fail("should prevent 0 buy amount");
-    } catch(err) {
-      const expectedError = "IntegratedLimitOrderDex: Amount is 0"
-      const actualError = err.reason;
-      assert.equal(actualError, expectedError, "should not be permitted")
-    }
-  });
-});
-*/
+
+// contract("IntegratedLimitOrderDex", (accounts) => {
+//   let dex;
+//   beforeEach(async () => {
+//     dex = await StubbedDex.new();
+//     (await dex.totalSupply.call()).should.be.eq.BN(await dex.balanceOf.call(accounts[0]));
+//     (await dex.totalSupply.call()).toString().should.be.equal("1000000000000000000");
+//     await dex.approve(dex.address, await dex.totalSupply.call());
+//   });
+
+//   it("should prevent cheating when buying", async () => {
+//     try {
+//       const price = web3.utils.toBN(web3.utils.toWei("1"));
+//       await dex.buy(1, price, {value: price.div(web3.utils.toBN(2))});
+//       assert.fail("should prevent cheating when value is not equal to amount purchased")
+//     } catch(err) {
+//       const expectedError = "IntegratedLimitOrderDex: Invalid message value"
+//       const actualError = err.reason;
+//       assert.equal(actualError, expectedError, "should not be permitted")
+//     }
+//   });
+
+//   it("should prevent cheating when selling", async () => {
+//     try {
+//       const price = web3.utils.toBN(web3.utils.toWei("1"));
+//       await dex.sell(1, price, {from: accounts[1]});
+//       assert.fail("should prevent cheating when value is not equal to amount purchased")
+//     } catch(err) {
+//       const expectedError = "ERC20: transfer amount exceeds balance"
+//       const actualError = err.reason;
+//       assert.equal(actualError, expectedError, "should not be permitted")
+//     }
+//   });
+
+//   it("should prevent overpaying when buying", async () => {
+//     try {
+//       const price = web3.utils.toBN(web3.utils.toWei("1"));
+//       await dex.buy(1, price, {value: price.mul(web3.utils.toBN(2))});
+//       assert.fail("should prevent overpaying when value is not equal to amount purchased")
+//     } catch(err) {
+//       const expectedError = "IntegratedLimitOrderDex: Invalid message value"
+//       const actualError = err.reason;
+//       assert.equal(actualError, expectedError, "should not be permitted")
+//     }
+//   });
+
+//   it("should prevent 0 buy price", async () => {
+//     try {
+//       const price = web3.utils.toWei("0.5")
+//       await dex.buy(1, 0, {value: 2 * price});
+//       assert.fail("should prevent 0 buy price");
+//     } catch(err) {
+//       const expectedError = "IntegratedLimitOrderDex: Price is 0"
+//       const actualError = err.reason;
+//       assert.equal(actualError, expectedError, "should not be permitted")
+//     }
+//   });
+
+//   it("should prevent 0 buy amount", async () => {
+//     try {
+//       const price = web3.utils.toWei("0.5")
+//       await dex.buy(0, price, {value: 2 * price});
+//       assert.fail("should prevent 0 buy amount");
+//     } catch(err) {
+//       const expectedError = "IntegratedLimitOrderDex: Amount is 0"
+//       const actualError = err.reason;
+//       assert.equal(actualError, expectedError, "should not be permitted")
+//     }
+//   });
+
+//   it("should prevent 0 sell price", async () => {
+//     try {
+//       await dex.sell(1, 0);
+//       assert.fail("should prevent 0 buy price");
+//     } catch(err) {
+//       const expectedError = "IntegratedLimitOrderDex: Price is 0"
+//       const actualError = err.reason;
+//       assert.equal(actualError, expectedError, "should not be permitted")
+//     }
+//   });
+
+//   it("should prevent 0 sell amount", async () => {
+//     try {
+//       const price = web3.utils.toWei("0.5")
+//       await dex.buy(0, price);
+//       assert.fail("should prevent 0 buy amount");
+//     } catch(err) {
+//       const expectedError = "IntegratedLimitOrderDex: Amount is 0"
+//       const actualError = err.reason;
+//       assert.equal(actualError, expectedError, "should not be permitted")
+//     }
+//   });
+// });
+
