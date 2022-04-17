bd6d7e67271f4074c723f637e8ba163f8c766666
Tom Wilson <tom.wilson87@abine.us>
2022-04-16 20:44:54 -0400

    Complete SingleBeacon test

--- contracts/beacon/SingleBeacon.sol
+++ contracts/beacon/SingleBeacon.sol
@@ -5,8 +5,7 @@ import "./Beacon.sol";
 
 // A pointless Beacon which reduces a BeaconProxy to a normal upgradable proxy
 // Maintains consistency of using BeaconProxys which may have benefits in the future
-// This can be chained with a regular Beacon to become a regular Beacon which
-// BeaconProxys could actually take advantage of
+// This can be also chained with a regular Beacon to become one, as it will still forward
 contract SingleBeacon is Beacon {
   constructor(bytes32 beaconName) Beacon(beaconName, 1) {}
 
--- test/beacon/SingleBeacon.test.js
+++ test/beacon/SingleBeacon.test.js
@@ -1,9 +1,35 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+let signer, auction, beacon;
+
 describe("SingleBeacon", () => {
   before(async () => {
+    signer = (await ethers.getSigners())[0];
 
+    // This needs to have a valid piece of code and Auction is trivial to deploy
+    auction = (await (await ethers.getContractFactory("Auction")).deploy()).address;
+    const SingleBeacon = await ethers.getContractFactory("SingleBeacon");
+    beacon = await SingleBeacon.deploy(ethers.utils.id("Auction"));
   });
 
-  it("should have tests", async () => {
-    throw "Untested";
+  it("should have the right amount of release channels", async () => {
+    expect(await beacon.releaseChannels()).to.equal(1);
+  });
+
+  it("should allow upgrading release channel 0", async () => {
+    let tx = await beacon.upgrade("0x0000000000000000000000000000000000000000", auction, 2, "0x");
+    expect(tx).to.emit("Upgrade").withArgs("0x0000000000000000000000000000000000000000", auction, 2, "0x");
+  });
+
+  it("should only allow upgrading release channel 0", async () => {
+    await expect(
+      beacon.upgrade("0x0000000000000000000000000000000000000001", auction, 2, "0x")
+    ).to.be.revertedWith(`UpgradingInstance("0x0000000000000000000000000000000000000001")`);
+
+    // Use ourselves as the instance as we do have authority to set our own code
+    await expect(
+      beacon.upgrade(signer.address, auction, 2, "0x")
+    ).to.be.revertedWith(`UpgradingInstance("${signer.address}")`);
   });
 });
