508f22235200b600d0148ec8c9e8d77df408cf5a
Tom Wilson <tom.wilson87@abine.us>
2022-04-22 01:36:53 -0400

    Test the DEXRouter

--- test/erc20/DEXRouter.test.js
+++ test/erc20/DEXRouter.test.js
@@ -1,9 +1,32 @@
-describe("DEXRouter", () => {
-  before(async () => {
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
+const FrabricERC20 = require("../../scripts/deployFrabricERC20.js");
+const { OrderType } = require("../common.js");
+
+let deployer;
+let usd, frbc, router;
+
+describe("DEXRouter", accounts => {
+  before(async () => {
+    signers = await ethers.getSigners();
+    deployer = signers.splice(0, 1)[0];
+
+    usd = await (await ethers.getContractFactory("TestERC20")).deploy("USD Test", "USD");
+    ({ frbc } = await FrabricERC20.deployFRBC(usd.address));
+    await frbc.mint(deployer.address, ethers.utils.parseUnits("1"));
+    router = (await (await ethers.getContractFactory("DEXRouter")).deploy());
   });
 
-  it("should have tests", async () => {
-    throw "Untested";
+  it("should be able to place buy orders", async () => {
+    await usd.approve(router.address, ethers.constants.MaxUint256);
+    let tx = await router.buy(frbc.address, usd.address, 2, 2, 1);
+    await expect(tx).to.emit(usd, "Transfer").withArgs(deployer.address, frbc.address, 2);
+    await expect(tx).to.emit(frbc, "NewOrder").withArgs(OrderType.Buy, 2);
+    await expect(tx).to.emit(frbc, "OrderIncrease").withArgs(deployer.address, 2, 1);
+  });
+
+  it("should use the msg.sender as the trader", async () => {
+    expect(await frbc.getOrderTrader(2, 0)).to.equal(deployer.address);
   });
 });
