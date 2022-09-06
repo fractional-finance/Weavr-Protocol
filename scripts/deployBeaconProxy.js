4a1aeee8ccbbaced530f0c7cf4ae76b601cb2f54
zeryx <1892175+zeryx@users.noreply.github.com>
2022-09-06 14:58:15 -0300

    added back tenderly support

--- scripts/deployBeaconProxy.js
+++ scripts/deployBeaconProxy.js
@@ -3,6 +3,9 @@ const { upgrades } = require("hardhat");
 // Support overriding the Beacon. It's generally Beacon yet may be SingleBeacon
 module.exports = async (beaconAddress, contractFactory, args, opts) => {
     let beaconProxy;
+    if (process.env.TENDERLY){
+        contractFactory = contractFactory.nativeContractFactory;
+    }
     if (opts == null) {
         beaconProxy = await upgrades.deployBeaconProxy(beaconAddress, contractFactory, args);
     } else {
