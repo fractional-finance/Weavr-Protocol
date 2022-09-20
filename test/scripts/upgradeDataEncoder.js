9918e60bc32bd22736bec4f6dc66ef29c3e1136c
abstrucked <mxhgraphics@gmail.com>
2022-09-20 20:44:47 +0200

    added some check on the upgradeDataEncoder.js script

--- test/scripts/upgradeDataEncoder.js
+++ test/scripts/upgradeDataEncoder.js
@@ -1,20 +1,31 @@
+const { ethers } = require("hardhat");
+const { ERRORS } = require("hardhat/internal/core/errors-list");
+
 const CONTRACTS = {
-    BOND: process.env.BOND || "",
-    THREAD_DEPLOYER: process.env.THREAD_DEPLOYER || ""
+    BOND: process.env.BOND || "CUSTOM_CONTRACT_ADDRESS",
+    THREAD_DEPLOYER: process.env.THREAD_DEPLOYER || "CUSTOM_CONTRACT_ADDRESS"
 }
 
 MEMBERS = {
-    SIGNER: "0x4e022c910339f320f08817dc0596a92044913C02",
+    SIGNER: "0x4e022c910339f320f08817dc0596a92044",
     GOVERNOR: "0x901268a9AEB7fcFaA5D667C52a9790AE6C64713e"
 }
 
 function upgradeData () {
-    const data = ethers.utils.defaultAbiCoder.encode(
-        ["address", "address", "address", "address"],
-        [CONTRACTS.BOND, CONTRACTS.THREAD_DEPLOYER, MEMBERS.SIGNER, MEMBERS.GOVERNOR],
-      );
-    console.log("DATA:\n", data);
-    return data;
+    if(
+        ethers.utils.isAddress(CONTRACTS.BOND) &&
+        ethers.utils.isAddress(CONTRACTS.THREAD_DEPLOYER) &&
+        ethers.utils.isAddress(MEMBERS.GOVERNOR) &&
+        ethers.utils.isAddress(MEMBERS.SIGNER)
+    ) {
+        const data = ethers.utils.defaultAbiCoder.encode(
+            ["address", "address", "address", "address"],
+            [CONTRACTS.BOND, CONTRACTS.THREAD_DEPLOYER, MEMBERS.SIGNER, MEMBERS.GOVERNOR],
+          );
+        console.log("DATA:\n", data);
+        return data;
+    }
+    throw new Error("SOME ADDRESS IS NOT PROPERLY SETUP")
 }
 
 upgradeData();
