82d05e9a3fd35d8d4add2730ea090df248ded8e2
zeryx <1892175+zeryx@users.noreply.github.com>
2022-09-25 00:20:29 -0300

    config settings imported

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
 require("solidity-coverage");
 
+
 module.exports = {
   solidity: {
     version: "0.8.9",
@@ -18,9 +19,14 @@ module.exports = {
   networks: {}
 };
 
-if (process.env.RINKEBY) {
-  module.exports.networks.rinkeby = {
-    url: process.env.RINKEBY,
+if (process.env.TENDERLY){
+  const tdly = require("@tenderly/hardhat-tenderly");
+  tdly.setup();
+}
+
+if (process.env.GOERLI) {
+  module.exports.networks.goerli = {
+    url: process.env.GOERLI,
     accounts: [
       process.env.PRIVATE_KEY
     ]
